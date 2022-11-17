80d0e7bc7e83a54e6ef52afac8091f19374ab1cf
mmm <aaa123@pobox.sk>
2022-11-17 16:32:05 +0100

    added MathSolver dependency

---
+++ build/MathSolver/CMakeFiles/MathSolver.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mmm/Plocha/IndustrialCraft/Calculator/MathSolver/EquationSystem.cpp" "/home/mmm/Plocha/IndustrialCraft/Calculator/build/MathSolver/CMakeFiles/MathSolver.dir/EquationSystem.cpp.o"
+  "/home/mmm/Plocha/IndustrialCraft/Calculator/MathSolver/Expression.cpp" "/home/mmm/Plocha/IndustrialCraft/Calculator/build/MathSolver/CMakeFiles/MathSolver.dir/Expression.cpp.o"
+  "/home/mmm/Plocha/IndustrialCraft/Calculator/MathSolver/ExpressionPair.cpp" "/home/mmm/Plocha/IndustrialCraft/Calculator/build/MathSolver/CMakeFiles/MathSolver.dir/ExpressionPair.cpp.o"
+  "/home/mmm/Plocha/IndustrialCraft/Calculator/MathSolver/ExpressionReader.cpp" "/home/mmm/Plocha/IndustrialCraft/Calculator/build/MathSolver/CMakeFiles/MathSolver.dir/ExpressionReader.cpp.o"
+  "/home/mmm/Plocha/IndustrialCraft/Calculator/MathSolver/Fraction.cpp" "/home/mmm/Plocha/IndustrialCraft/Calculator/build/MathSolver/CMakeFiles/MathSolver.dir/Fraction.cpp.o"
+  "/home/mmm/Plocha/IndustrialCraft/Calculator/MathSolver/Term.cpp" "/home/mmm/Plocha/IndustrialCraft/Calculator/build/MathSolver/CMakeFiles/MathSolver.dir/Term.cpp.o"
+  "/home/mmm/Plocha/IndustrialCraft/Calculator/MathSolver/Variable.cpp" "/home/mmm/Plocha/IndustrialCraft/Calculator/build/MathSolver/CMakeFiles/MathSolver.dir/Variable.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../MathSolver"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/MathSolver/CMakeFiles/MathSolver.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathSolver.dir/EquationSystem.cpp.o"
+  "CMakeFiles/MathSolver.dir/Expression.cpp.o"
+  "CMakeFiles/MathSolver.dir/ExpressionPair.cpp.o"
+  "CMakeFiles/MathSolver.dir/ExpressionReader.cpp.o"
+  "CMakeFiles/MathSolver.dir/Fraction.cpp.o"
+  "CMakeFiles/MathSolver.dir/Term.cpp.o"
+  "CMakeFiles/MathSolver.dir/Variable.cpp.o"
+  "libMathSolver.a"
+  "libMathSolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
